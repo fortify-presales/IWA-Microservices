4ef95df9c2e6e061e09e9e6392470b9c136d15b3
kadraman <klee2@opentext.com>
2025-04-23 16:59:17 +0100

    Updating Actions

--- user-service/src/config/index.js
+++ user-service/src/config/index.js
@@ -8,11 +8,11 @@ if (process.env.NODE_ENV == "prod") {
 }
 
 module.exports = {
-  PORT: process.env.PORT,
-  DB_URL: process.env.MONGODB_URI,
-  APP_SECRET: process.env.APP_SECRET,
-  EXCHANGE_NAME: process.env.EXCHANGE_NAME,
-  MSG_QUEUE_URL: process.env.MSG_QUEUE_URL,
+  PORT: process.env.PORT || 5000,
+  DB_URL: process.env.MONGODB_URI || "mongodb://nosql-db/iwaUserDB",
+  APP_SECRET: process.env.APP_SECRET || "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiIxMjM0NTY3ODkwIiwibmFtZSI6IkpvaG4gRG9lIiwiaWF0IjoxNTE2MjM5MDIyfQ.SflKxwRJSMeKKF2QT4fwpMeJf36POk6yJV_adQssw5c",
+  EXCHANGE_NAME: process.env.EXCHANGE_NAME || "IWA_STORE",
+  MSG_QUEUE_URL: process.env.MSG_QUEUE_URL || "amqp://rabbitmq:5672",
   USER_SERVICE: "user_service",
   STORE_SERVICE: "store_service",
 };
